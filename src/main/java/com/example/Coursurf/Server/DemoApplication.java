d1d440ae9d05cda29bd75d9e5fff6e17a29dc1b7
Sakthe-Balan <sakthebalan2003@gmail.com>
2024-03-22 19:47:04 +0530

    Added index.html page

---
+++ src/main/java/com/example/Coursurf/Server/DemoApplication.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/demo/DemoApplication.java
+package com.example.Coursurf.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
